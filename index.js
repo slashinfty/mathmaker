fde6e9701321fcefd566a3a6d6dc6c9238b3f708
Matt Braddock <matt.braddock@gmail.com>
2025-02-04 16:15:23 -0500

    quadratic partial

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+// MathJax initialization
+MathJax = {
+    tex: {
+        inlineMath: [
+            ['$', '$']
+        ]
+    }
+}
+
+// Utility functions
+function randomInt(min, max) {
+    return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min));
+}
+
 function factors(n) {
     let a = [1, n];
     for (let i = 2; i <= Math.floor(Math.sqrt(n)); i++) {
@@ -23,11 +37,50 @@ function lcm(a) {
 }
 
 function sqrt(n) {
-    if (Number.isInteger(Math.sqrt(n))) return [Math.sqrt(n), 1];
+    if (Number.isInteger(Math.sqrt(n))) return `${Math.sqrt(n)}`;
     let s = Math.floor(Math.sqrt(n / 2));
     do {
-        if (n % Math.pow(s, 2) === 0) return [s, n / Math.pow(s, 2)];
+        if (n % Math.pow(s, 2) === 0) return `${s}\sqrt{${n / Math.pow(s, 2)}}`;
         s--;
     } while (s > 0);
-    return [1, n];
+    return `\sqrt{${n}}`;
 }
+
+function frac(n, d) {
+    if (n % d === 0) return `${n / d}`;
+    return `${(n > 0 && d < 0) || (n < 0 && d > 0) ? '-' : ''}\dfrac{${Math.abs(n / gcd([n, d]))}}{${Math.abs(d / gcd([n, d]))}}`;
+}
+
+// Add question
+function question(element, question, solution) {
+    // use a class name to denote active question
+    
+    // find old element, if it exists
+    // remove inner html and class
+    // add class to new element
+    // add question, solution, and link to show solution
+}
+
+// Generators
+document.querySelector('#quadratic').addEventListener('click', () => {
+    const type = Number(document.querySelector('input[name="quadratic-type"]:checked').value);
+    if (type < 3) {
+        const [p, q] = [randomInt(-15, 15), randomInt(-15, 15)];
+        const [r, s] = [type === 2 ? randomInt(1, 4) : 1, type === 2 ? randomInt(1, 4) : 1];
+        const [a, b, c, d] = [r / gcd([r, p]), p / gcd([r, p]), s / gcd([s, q]), q / gcd([s, q])];
+        let [h, k] = [1, 1];
+        if (document.querySelector('quadratic-larger').checked) {
+            k = randomInt(1, 6);
+            if (type === 2) h = randomInt(1, 6);
+        }
+        const g = document.querySelector('quadratic-gcf').checked ? randomInt(1, 12) : 1;
+        const [x, y, z] = [a * c * g * Math.pow(h, 2), -1 * g * (a * d * h * k + b * c * h * k), b * d * g * Math.pow(k, 2)];
+        question(
+            document.querySelector('#quadratic-placement'),
+            `$$${x}x^2${y > 0 ? '+' : ''}${y === 0 ? '' : `${y}x`}${z > 0 ? '+' : ''}${z === 0 ? '' : z}=0$$`,
+            `$$x=${frac(k * b, a * h)},x=${frac(k * d, c * h)}$$`
+        );
+    } else {
+
+    }
+});
